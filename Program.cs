0742ec742a40c7c70c409540794198fd3a861873
Akash Chaudhary <akash.chaudhary@softlineindia.onmicrosoft.com>
2023-02-28 17:37:37 +0530

    OpenAPI defination for API management

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+//imported OpenApi model
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,10 +8,23 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//AC- added OpenAPI Info
+builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Contoso Pizza", Version = "v1" });
+    }
+    );
 
 var app = builder.Build();
-
+//AC - Added swagger request handling
+app.UseSwagger(c =>
+{
+    c.PreSerializeFilters.Add((swagger, httpReq) => {
+        var server = new OpenApiServer() { Url = $"{httpReq.Scheme}://{httpReq.Host.Value}" };
+        swagger.Servers = new List<OpenApiServer>() { server };
+    });
+});
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ContosoPizza.ApiApp v1"));
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
